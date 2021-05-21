f53eb1ea706f80c18223824cfc16821c64124c95
rdi00 <61155306+rdi00@users.noreply.github.com>
2021-05-21 17:49:12 +0100

    ecra login

---
+++ app/src/main/java/ipvc/estg/olxcm/Login.kt
@@ -0,0 +1,12 @@
+package ipvc.estg.olxcm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ipvc.estg.olxcm.R
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
